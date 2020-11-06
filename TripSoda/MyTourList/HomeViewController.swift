259176dbc86db99daad31bff0cf41b0987e1eb4b
sanghun0724 <https://github.com/sanghun0724>
2020-11-06 23:58:15 +0900

    not

--- TripSoda/MyTourList/HomeViewController.swift
+++ TripSoda/MyTourList/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController,UINavigationControllerDelegate {
+class HomeViewController: UIViewController, {
 
     override func viewDidLoad() {
         super.viewDidLoad()
