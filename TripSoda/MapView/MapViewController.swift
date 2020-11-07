05a00b919ec2fcc6e63b0f5774cdc0b9d7b73134
sanghun0724 <sanghun0724@naver.com>
2020-11-08 02:01:52 +0900

    d

--- TripSoda/MapView/MapViewController.swift
+++ TripSoda/MapView/MapViewController.swift
@@ -8,32 +8,71 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController ,CLLocationManagerDelegate{
 
+    @IBOutlet var myMap:MKMapView!
     
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        locationManager.delegate = self
+        //정확도 최고로
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        myMap.showsUserLocation = true
     }
     
+    // 위도와 경도, 스팬(영역 폭)을 입력받아 지도에 표시
+        func goLocation(latitudeValue: CLLocationDegrees,
+                        longtudeValue: CLLocationDegrees,
+                        delta span: Double) -> CLLocationCoordinate2D {
+            let pLocation = CLLocationCoordinate2DMake(latitudeValue, longtudeValue)
+            let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+            let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+            myMap.setRegion(pRegion, animated: true)
+            return pLocation
+        }
 
-
-}override func viewDidLoad() {
-    super.viewDidLoad()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }
+    // 특정 위도와 경도에 핀 설치하고 핀에 타이틀과 서브 타이틀의 문자열 표시
+        func setAnnotation(latitudeValue: CLLocationDegrees,
+                           longitudeValue: CLLocationDegrees,
+                           delta span :Double,
+                           title strTitle: String,
+                           subtitle strSubTitle:String){
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = goLocation(latitudeValue: latitudeValue, longtudeValue: longitudeValue, delta: span)
+            annotation.title = strTitle
+            annotation.subtitle = strSubTitle
+            myMap.addAnnotation(annotation)
+        }
     
+    // 위치 정보에서 국가, 지역, 도로를 추출하여 레이블에 표시
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            let pLocation = locations.last
+            _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!,
+                       longtudeValue: (pLocation?.coordinate.longitude)!,
+                       delta: 0.01)
+            CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {(placemarks, error) -> Void in
+                let pm = placemarks!.first
+                let country = pm!.country
+                var address: String = ""
+                if country != nil {
+                    address = country!
+                }
+                if pm!.locality != nil {
+                    address += " "
+                    address += pm!.locality!
+                }
+                if pm!.thoroughfare != nil {
+                    address += " "
+                    address += pm!.thoroughfare!
+                }
+               
+            })
+            locationManager.stopUpdatingLocation()
+        }
+
 }
